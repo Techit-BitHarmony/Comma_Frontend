41c13895e3f242580bbf8c65915cf8f8ccbe82f4
ppusda <ppusda1234@gmail.com>
2024-02-08 19:35:03 +0900

    feat: Login/Logout 구현

--- src/lib/components/store.js
+++ src/lib/components/store.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 export const audioTrack = writable(null);
 
-export const loginUsername = writable('');
+export const loginUsername = writable('프로필');
 export const isLogin = writable(false);
 export const baseUrl = writable("http://localhost:8090");
 //export const baseUrl = writable("https://yonguk.shop");
