ee91cff58f1997ef755d288397004a7d6d29f64a
Sanghyuk Park <dropthegit@gmail.com>
2024-02-14 01:49:29 +0900

    Article, Comment Create, Modify, Delete

--- src/routes/(app)/member/[username]/community/+page.svelte
+++ src/routes/(app)/member/[username]/community/+page.svelte
@@ -1,19 +1,20 @@
-<script lang="ts">
+<script>
 	import { onMount } from 'svelte';
-	import { writable } from 'svelte/store'; // Svelte store를 사용하여 페이지 번호를 관리합니다.
+	import { writable } from 'svelte/store';
+	import { page } from '$app/stores';
+	import CommentList from './CommentList.svelte';
+	import DeleteButton from './DeleteButton.svelte';
 
-	let articles = writable([]); // 페이지네이션에 사용할 게시글 데이터를 저장하는 store
-	let currentPage = writable(1); // 현재 페이지 번호를 저장하는 store
-	let totalPages = writable(1); // 전체 페이지 수를 저장하는 store
-	let itemsPerPage = 10; // 페이지당 항목 수
+	let articles = writable([]);
+	let currentPage = writable(1);
+	let totalPages = writable(1);
+	let itemsPerPage = 10;
 	let totalElements = '';
 
 	onMount(async () => {
-		// 페이지 로드 시 데이터를 불러옵니다.
 		loadArticles();
 	});
 
-	// 페이지네이션을 위한 함수
 	async function loadArticles() {
 		try {
 			const accessToken = document.cookie
@@ -27,7 +28,7 @@
 
 			// 현재 페이지와 페이지당 항목 수를 기반으로 데이터를 가져옵니다.
 			const response = await fetch(
-				`http://localhost:8090/community/articles?page=${$currentPage}`,
+				`http://localhost:8090/community/articles/user/${$page.params.username}?page=${$currentPage}`,
 				{
 					method: 'GET',
 					headers: {
@@ -52,65 +53,127 @@
 		}
 	}
 
-	// 이전 페이지로 이동하는 함수
 	function previousPage() {
-		currentPage.update((n) => Math.max(n - 1, 1)); // 현재 페이지를 업데이트합니다.
-		loadArticles(); // 새로운 페이지의 데이터를 불러옵니다.
+		currentPage.update((n) => Math.max(n - 1, 1));
+		loadArticles();
 	}
 
-	// 다음 페이지로 이동하는 함수
 	function nextPage() {
-		currentPage.update((n) => n + 1); // 현재 페이지를 업데이트합니다.
-		loadArticles(); // 새로운 페이지의 데이터를 불러옵니다.
+		currentPage.update((n) => n + 1);
+		loadArticles();
 	}
 
-	function movePage(pageNumber: any) {
-		currentPage.set(pageNumber); // 현재 페이지를 업데이트합니다.
-		loadArticles(); // 새로운 페이지의 데이터를 불러옵니다.
+	function movePage(pageNumber) {
+		currentPage.set(pageNumber);
+		loadArticles();
+	}
+
+	function scrollToArticle(articleId) {
+		console.log('Scrolling to article:', articleId);
+		window.location.hash = `#article-${articleId}`;
+	}
+
+	async function submitComment(event) {
+		event.preventDefault(); // 폼의 기본 동작인 페이지 새로고침을 막습니다.
+
+		const form = event.target;
+		const articleId = form.elements['articleId'].value;
+		const content = form.elements['content'].value;
+
+		try {
+			const accessToken = document.cookie
+				.split('; ')
+				.find((row) => row.startsWith('accessToken='))
+				?.split('=')[1];
+
+			if (!accessToken) {
+				throw new Error('AccessToken이 없습니다.');
+			}
+
+			const response = await fetch('http://localhost:8090/community/comments', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `${accessToken}`
+				},
+				body: JSON.stringify({ articleId, content })
+			});
+
+			if (!response.ok) {
+				throw new Error('네트워크 오류: ' + response.statusText);
+			}
+
+			scrollToArticle(articleId);
+			form.reset();
+			location.reload();
+		} catch (error) {
+			console.error('댓글을 작성하는 중 오류 발생:', error);
+		}
 	}
 </script>
 
 <div class="container my-4 space-y-4">
-	<h1>커뮤니티</h1>
+	<div class="flex justify-between items-center my-6 mb-4">
+		<h1>커뮤니티</h1>
+		<a class="btn btn-primary" href="./community/write">글쓰기</a>
+	</div>
 
 	{#each $articles as article, index}
 		<div class="card w-auto bg-base-100 shadow-xl">
 			<div class="card-body">
 				<div class="flex justify-between">
-					<div class="flex justify-between items-center">
-						<h2 class="card-title">{article.title}</h2>
-						<span class="badge ml-2"
-							>{new Date(article.createDate).toLocaleDateString('ko-KR', {
-								year: 'numeric',
-								month: '2-digit',
-								day: '2-digit',
-								hour: '2-digit',
-								minute: '2-digit'
-							})}</span
-						>
+					<div class="flex-col items-center">
+						<h2 class="card-title mb-2">{article.title}</h2>
+						<div>
+							<span>작성자 {article.username}</span>
+							<span class="badge ml-2"
+								>{new Date(article.createDate).toLocaleDateString('ko-KR', {
+									year: 'numeric',
+									month: '2-digit',
+									day: '2-digit',
+									hour: '2-digit',
+									minute: '2-digit'
+								})}</span
+							>
+						</div>
 					</div>
 					<div class="dropdown">
 						<div tabindex="0" role="button" class="btn btm-xs">. . .</div>
-						<ul
-							tabindex="0"
-							class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-						>
-							<li><a>수정하기</a></li>
-							<li><a>삭제하기</a></li>
+						<ul class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+							<li><a href='./community/modify?articleId={article.id}'>수정하기</a></li>
+							<li><DeleteButton articleId={article.id} /></li>
 						</ul>
 					</div>
 				</div>
-				<div class="border-b border-gray-300 pb-1 mb-4" />
+				<div class="divider divider-neutral"></div>
 				<p>{article.content}</p>
-				<div class="card-actions justify-end">
-					<button class="btn btn-primary">댓글 작성하기</button>
-				</div>
-                <div class="border-b border-gray-300 pb-1 mt-4 mb-4" />
+				<div class="divider mt-20">댓글</div>
+
+				<details class="collapse bg-base-200">
+					<summary class="collapse-title text-sm font-medium">댓글 작성하기</summary>
+					<div class="collapse-content">
+						<form id="commentForm" on:submit={submitComment}>
+							<div>
+								<input
+									class="input input-bordered input-md w-full max-w-xs"
+									type="text"
+									name="content"
+									placeholder="댓글을 작성하세요."
+								/>
+								<input type="hidden" name="articleId" value={article.id} />
+								<button type="submit" class="btn btn-primary">작성</button>
+							</div>
+						</form>
+					</div>
+				</details>
+			</div>
+			<div>
+				<CommentList articleId={article.id} />
 			</div>
 		</div>
 	{/each}
 
-	<div class="join">
+	<div class="join flex justify-center">
 		<button class="join-item btn btn-square" on:click={previousPage}>이전</button>
 		{#each Array.from({ length: $totalPages }, (_, index) => index + 1) as pageNumber}
 			{#if pageNumber === $currentPage}
