e122218e4ef868f5bdcf376723cd260dfded7c7e
yonguk <kimyu9827@naver.com>
2024-02-14 13:07:59 +0900

    feature: donation 조회, 등록, 정기후원 등록,수정,삭제

--- src/lib/components/token.js
+++ src/lib/components/token.js
@@ -1,7 +1,7 @@
 import { isLogin } from "$components/store.js";
 import {goto} from "$app/navigation";
 
-function getCookie(name) {
+export function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
