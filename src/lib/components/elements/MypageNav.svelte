ea5d4de5580416a9e43c8ef729cf5b0116af3a79
changhyun9 <ckdgus1713@naver.com>
2024-02-14 13:12:58 +0900

    refactor : MypageNav loginUsername 추가
    MypageNav - loginUsername 전역변수 추가

--- src/lib/components/elements/MypageNav.svelte
+++ src/lib/components/elements/MypageNav.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte';
+  import {loginUsername} from "$components/store.js"
 
   let currentUrl = '';
 
@@ -10,24 +11,22 @@
 
 <div class="flex flex-row mb-3">
     <div class="flex mr-3 ml-3 pr-2 border-r border-gray-500">
-        <a  class:highlight={currentUrl === '/member/user1/modify'} href="/member/user1/modify">마이페이지</a>
+        <a class:highlight={currentUrl === `/member/${$loginUsername}/modify`} href="/member/{$loginUsername}/modify">마이페이지</a>
     </div>
     <div class="flex mr-3 pr-2 border-r border-gray-500">
-        <a class:highlight={currentUrl === '/member/user1/modify/password'} href="/member/user1/modify/password">비밀번호 변경</a>
+        <a class:highlight={currentUrl === `/member/${$loginUsername}/modify/password`} href="/member/{$loginUsername}/modify/password">비밀번호 변경</a>
     </div>
     <div class="flex mr-3 pr-2 border-r border-gray-500">
-        <a class:highlight={currentUrl === '/'} href="/">크레딧</a>
+        <a class:highlight={currentUrl === `/member/${$loginUsername}/credit`} href="/member/{$loginUsername}/credit">크레딧</a>
     </div>
     <div class="flex mr-3">
-        <a class:highlight={currentUrl === '/'} href="/">커뮤니티</a>
+        <a class:highlight={currentUrl === `/member/${$loginUsername}/community`} href="/member/{$loginUsername}/community">커뮤니티</a>
     </div>
 </div>
 
 <style>
     
     .highlight:visited {
-    /* 현재 URL과 일치하는 링크의 스타일 */
-    color: white; /* 원하는 색상으로 변경 */
-    /* 추가적인 스타일 적용 가능 */
+    color: white;
   }
   </style>
