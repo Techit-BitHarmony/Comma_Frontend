7fd83bb204fca6799a007f30702fdebcf4c72aab
yonguk <kimyu9827@naver.com>
2024-02-15 18:00:38 +0900

    fix: base url, deploy server address

--- src/lib/components/store.js
+++ src/lib/components/store.js
@@ -5,5 +5,5 @@ export const audioTrack = writable(null);
 export const loginUsername = writable('프로필');
 export const loginMemberId = writable('0');
 export const isLogin = writable(false);
-export const baseUrl = writable("http://localhost:8090");
+export const baseUrl = writable("https://yonguk.shop");
 //export const baseUrl = writable("https://yonguk.shop");
