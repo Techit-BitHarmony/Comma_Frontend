f8e687f4516f5d3af49eba3f8026c53f1929860f
Sanghyuk Park <dropthegit@gmail.com>
2024-02-15 10:42:41 +0900

    Witdraws List Pagination Done

--- src/routes/(app)/credit/Withdraws.svelte
+++ src/routes/(app)/credit/Withdraws.svelte
@@ -1,54 +1,83 @@
 <!-- WithdrawalLogs.svelte -->
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { getCookie } from '$components/token.js';
 	import { baseUrl } from '$components/store.js';
 	import { toastNotice } from '$components/toastr';
 	import { toastWarning } from '$components/toastr';
 
-	export let withdraws: any[] = [];
+	let withdraws = writable([]);
+	let currentPage = writable(1);
+	let totalPages = writable(1);
+	let itemsPerPage = 10;
+	let totalElements = '';
 
 	onMount(async () => {
 		try {
-			loadWithdraws(); 
+			loadWithdraws();
 		} catch (error) {
 			toastWarning('정보를 불러오는 데에 실패하였습니다.');
 		}
 	});
 
-	
-
-	async function loadWithdraws(){
+	async function loadWithdraws() {
 		const accessToken = getCookie('accessToken');
 
-			if (!accessToken) {
-				toastWarning('로그인 해주세요.');
-				return;
-			}
+		if (!accessToken) {
+			toastWarning('로그인 해주세요.');
+			return;
+		}
 
-			const withdrawsResponse = await fetch($baseUrl +'/credit/withdraws/mine', {
+		const withdrawsResponse = await fetch(
+			$baseUrl + `/credit/withdraws/mine?page=${$currentPage}`,
+			{
 				method: 'GET',
 				credentials: 'include',
 				headers: {
 					'Content-Type': 'application/json',
 					Authorization: `${accessToken}`
 				}
-			});
-
-			const withdrawsResp = await withdrawsResponse.json();
-
-			if (!withdrawsResponse.ok) {
-				toastWarning(withdrawsResp.message);
 			}
+		);
 
-			withdraws = withdrawsResp.withdraws;
+		const withdrawsResp = await withdrawsResponse.json();
+
+		if (!withdrawsResponse.ok) {
+			toastWarning(withdrawsResp.message);
+		}
+
+		withdraws.set(withdrawsResp.withdraws.content);
+
+		const totalWithdraws = withdrawsResp.withdraws.totalElements;
+		totalElements = totalWithdraws;
+		totalPages.set(Math.ceil(totalWithdraws / itemsPerPage));
+	}
+
+	function previousPage() {
+		currentPage.update((n) => Math.max(n - 1, 1));
+		loadWithdraws();
+	}
+
+	function nextPage() {
+		currentPage.update((n) => n + 1);
+		loadWithdraws();
+	}
+
+	function movePage(pageNumber) {
+		currentPage.set(pageNumber);
+		loadWithdraws();
 	}
 </script>
 
 <div class="card bg-base-100 dark:bg-gray-800 p-4">
-	<p class="text-3xl text-primary-dark dark:text-primary font-extrabold mb-3"><i class="fa-solid fa-wallet me-3"></i>출금 신청 내역</p>
+	<p class="text-3xl text-primary-dark dark:text-primary font-extrabold mb-3">
+		<i class="fa-solid fa-wallet me-3" />출금 신청 내역
+	</p>
 	<table class="table">
-		<thead class="bg-gray-200 dark:bg-gray-600 text-center text-primary-dark dark:text-primary text-lg font-bold">
+		<thead
+			class="bg-gray-200 dark:bg-gray-600 text-center text-primary-dark dark:text-primary text-lg font-bold"
+		>
 			<tr>
 				<th>날짜</th>
 				<th>은행명</th>
@@ -58,7 +87,7 @@
 			</tr>
 		</thead>
 		<tbody class="text-center text-primary-dark dark:text-primary">
-			{#each withdraws as withdraw}
+			{#each $withdraws as withdraw}
 				<tr>
 					<td>{new Date(withdraw.applyDate).toLocaleDateString('ko-KR')}</td>
 					<td>{withdraw.bankName}</td>
@@ -83,7 +112,27 @@
 			{/each}
 		</tbody>
 	</table>
-	{#if withdraws.length === 0}
+	{#if $withdraws.length === 0}
 		<p class="flex justify-center">출금 신청 내역이 없습니다</p>
 	{/if}
+
+	<div class="join flex justify-center">
+		{#if $totalPages > 0}
+			<button class="join-item btn btn-square" on:click={previousPage}>이전</button>
+		{/if}
+		{#each Array.from({ length: $totalPages }, (_, index) => index + 1) as pageNumber}
+			{#if pageNumber === $currentPage}
+				<button class="join-item btn btn-square btn-active" on:click={() => movePage(pageNumber)}
+					>{pageNumber}</button
+				>
+			{:else}
+				<button class="join-item btn btn-square" on:click={() => movePage(pageNumber)}
+					>{pageNumber}</button
+				>
+			{/if}
+		{/each}
+		{#if $totalPages > $currentPage}
+			<button class="join-item btn btn-square" on:click={nextPage}>다음</button>
+		{/if}
+	</div>
 </div>
